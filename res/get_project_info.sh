3cad467960b8c513e048f51efaa6d9e48b87e27c
zhenquan.qiu <gnsyxiang@163.com>
2021-07-02 11:21:03 +0800

    feat: get project name and version from root dir without .git

--- res/get_project_info.sh
+++ res/get_project_info.sh
@@ -8,8 +8,9 @@ check_project_info=${m4_dir}/check_project_info.m4
 
 get_project_name()
 {
-    libproject_url=`git remote -v | head -n1 | awk '{print $2}' | sed 's/git@/https:\/\//' | sed 's/com:/com\//'`
-    libproject_name=`git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//'`
+    libproject_name=$1
+    libproject_url=$2
+
     project_name=${libproject_name#*lib}
     project_name_2=${libproject_name%lib*}
     echo -e "\e[32m\tproject_name: $libproject_name \e[0m"
@@ -32,17 +33,11 @@ get_project_name()
 
 get_full_version_num()
 {
-    if git describe --tags | sed s/-/_/g > version.num.tmp
-    then
-        mv version.num.tmp version.num
-    else
-        rm -rf version.num.tmp
-    fi
+    full_version=$1
 
-    full_version=`tr -d '\n' < version.num`
-    current=`cat version.num    | awk -F '_' '{print $1}' | awk -F '.' '{print $1}'`
-    revision=`cat version.num   | awk -F '_' '{print $1}' | awk -F '.' '{print $2}'`
-    age=`cat version.num        | awk -F '_' '{print $1}' | awk -F '.' '{print $3}'`
+    current=`echo $full_version    | awk -F '_' '{print $1}' | awk -F '.' '{print $1}'`
+    revision=`echo $full_version   | awk -F '_' '{print $1}' | awk -F '.' '{print $2}'`
+    age=`echo $full_version        | awk -F '_' '{print $1}' | awk -F '.' '{print $3}'`
 
     echo -e "\e[32m\tfull_version: $full_version \e[0m"
 
@@ -56,12 +51,36 @@ get_full_version_num()
     echo -e "\t\t\tAC_SUBST(revision)"                      >>  ${check_project_info}
     echo -e "\t\t\tAC_SUBST(age)"                           >>  ${check_project_info}
     echo -e "\t\t  ])"                                      >>  ${check_project_info}
-
-    rm -rf version.num
 }
 
-echo "### get project name..."
-get_project_name
 
-echo "### get version num..."
-get_full_version_num
+
+if [ -d $TOP_DIR/.git ]; then
+    echo "### get project name..."
+
+    libproject_url=`git remote -v | head -n1 | awk '{print $2}' | sed 's/git@/https:\/\//' | sed 's/com:/com\//'`
+    libproject_name=`git remote -v | head -n1 | awk '{print $2}' | sed 's/.*\///' | sed 's/\.git//'`
+
+    get_project_name $libproject_name $libproject_url
+
+    echo "### get version num..."
+    libproject_version=`git describe --tags | sed s/-/_/g`
+
+    get_full_version_num $libproject_version
+else
+    echo "### get project name..."
+    project_path=`pwd`
+    project_path_left=${project_path%/*}
+    project_path_right=${project_path_left##*/}
+
+    project_name=${project_path_right%-*}
+
+    get_project_name $project_name
+
+    echo "### get version num..."
+    project_version=${project_path_right#*-}
+
+    get_full_version_num $project_version
+fi
+
+
