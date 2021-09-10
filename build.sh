b716a4a4e9cefc19d63e0491c1132bf3a9d9e71d
zhenquan.qiu <gnsyxiang@163.com>
2021-09-10 08:19:46 +0800

    build: pass global variables for Makefile

--- build.sh
+++ build.sh
@@ -14,31 +14,50 @@ fi
 
 data_disk_path=/opt/data
 
+_cppflags_com=""
+_cflags_com=""
+_cxxflags_com=""
+_ldflag_com=""
+_param_com=""
+
+_cppflags_com="${_cppflags_com} -W -Wall -Werror"
+_cppflags_com="${_cppflags_com} -Wno-error=unused-parameter"
+_cppflags_com="${_cppflags_com} -Wno-error=unused-but-set-variable"
+_cppflags_com="${_cppflags_com} -Wno-error=unused-variable"
+_cppflags_com="${_cppflags_com} -Wno-error=unused-function"
+_cppflags_com="${_cppflags_com} -pipe"
+_cppflags_com="${_cppflags_com} -ffunction-sections"
+_cppflags_com="${_cppflags_com} -fdata-sections"
+_cppflags_com="${_cppflags_com} -fstack-protector-all"
+_ldflag_com="${_ldflag_com} -rdynamic"
+_ldflag_com="${_ldflag_com} -Wl,--gc-sections"
+_ldflag_com="${_ldflag_com} -Wl,--as-needed"
+
 if [ x$1 = x"pc" ]; then
     vender=pc
     gcc_version=x86_64-linux-gnu
-    _param_com=""
 elif [ x$1 = x"arm" ]; then
     vender=hisi
     host=arm-himix200-linux
     gcc_version=arm-himix200-linux
     gcc_prefix=arm-himix200-linux
     cross_gcc_path=${data_disk_path}/opt/toolchains/${vender}/${gcc_version}/bin/${gcc_prefix}-
-    _param_com=""
 elif [ x$1 = x"mcu" ]; then
     vender=gnu_arm_embedded
     host=arm-none-eabi
     gcc_version=gcc-arm-none-eabi-5_4-2016q3
     gcc_prefix=arm-none-eabi
     cross_gcc_path=${data_disk_path}/opt/toolchains/${vender}/${gcc_version}/bin/${gcc_prefix}-
-    _ldflag_com="-specs=nano.specs -specs=nosys.specs"
-    _param_com="--with-target_os=mcu"
+
+    _ldflag_com="${_ldflag_com} -specs=nano.specs -specs=nosys.specs"
 else
     help_info
 fi
 
 # 3rd_lib path
 lib_3rd_path=${data_disk_path}/install/${vender}/${gcc_version}
+_cppflags_com="${_cppflags_com} -I${lib_3rd_path}/include"
+_ldflag_com="${_ldflag_com} -L${lib_3rd_path}/lib"
 
 # target
 target_path=`pwd`
@@ -54,10 +73,10 @@ fi
 ${target_path}/configure                                    \
     CC=${cross_gcc_path}gcc                                 \
     CXX=${cross_gcc_path}g++                                \
-    CPPFLAGS="-I${lib_3rd_path}/include"                    \
-    CFLAGS=""                                               \
-    CXXFLAGS=""                                             \
-    LDFLAGS="-L${lib_3rd_path}/lib ${_ldflag_com}"          \
+    CPPFLAGS="${_cppflags_com} "                            \
+    CFLAGS="${_cflags_com}"                                 \
+    CXXFLAGS="${_cxxflags_com}"                             \
+    LDFLAGS="${_ldflag_com}"                                \
     LIBS=""                                                 \
     PKG_CONFIG_PATH="${lib_3rd_path}/lib/pkgconfig"         \
     --prefix=${prefix_path}                                 \
