1c161254a1ea8e2672a50700565fbd3d45917cdc
zhenquan.qiu <gnsyxiang@163.com>
2021-09-30 12:29:45 +0800

    build: add -Wno-error=unused-parameter

--- build.sh
+++ build.sh
@@ -21,7 +21,7 @@ _ldflag_com=""
 _param_com=""
 
 _cppflags_com="${_cppflags_com} -W -Wall -Werror"
-_cppflags_com="${_cppflags_com} -Wno-error=unused-parameter"
+_cppflags_com="${_cppflags_com} -Wno-unused-parameter"
 _cppflags_com="${_cppflags_com} -Wno-error=unused-but-set-variable"
 _cppflags_com="${_cppflags_com} -Wno-error=unused-variable"
 _cppflags_com="${_cppflags_com} -Wno-error=unused-function"
