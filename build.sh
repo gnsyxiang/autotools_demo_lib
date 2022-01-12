d8fd6a8c8f848e8f20ae8398a939d143a13ff4dc
zhenquan.qiu <gnsyxiang@163.com>
2022-01-12 16:02:04 +0800

    build: update build.sh with release/debug

--- build.sh
+++ build.sh
@@ -4,23 +4,17 @@
 
 help_info()
 {
-    echo "eg: ./build.sh pc/arm/mcu [_build]"
+    echo "eg: ./build.sh pc/arm/mcu release/debug [_build]"
     exit
 }
 
-if [ $# -gt 2 -o $# -lt 1 ]; then
+if [ $# -gt 3 -o $# -lt 2 ]; then
     help_info
 fi
 
 data_disk_path=/opt/data
 
-_cppflags_com=""
-_cflags_com=""
-_cxxflags_com=""
-_ldflag_com=""
-_param_com=""
-
-_cppflags_com="${_cppflags_com} -W -Wall -Werror"
+_cppflags_com="-W -Wall -Werror"
 _cppflags_com="${_cppflags_com} -Wno-unused-parameter"
 _cppflags_com="${_cppflags_com} -Wno-error=unused-but-set-variable"
 _cppflags_com="${_cppflags_com} -Wno-error=unused-variable"
@@ -30,10 +24,16 @@ _cppflags_com="${_cppflags_com} -ffunction-sections"
 _cppflags_com="${_cppflags_com} -fdata-sections"
 _cppflags_com="${_cppflags_com} -fstack-protector-all"
 
-_ldflag_com="${_ldflag_com} -rdynamic"
+_cflags_com=""
+
+_cxxflags_com=""
+
+_ldflag_com="-rdynamic"
 _ldflag_com="${_ldflag_com} -Wl,--gc-sections"
 _ldflag_com="${_ldflag_com} -Wl,--as-needed"
 _ldflag_com="${_ldflag_com} -Wl,-rpath=../lib"
+
+_param_com=""
 
 if [ x$1 = x"pc" ]; then
     vender=pc
@@ -56,6 +56,12 @@ else
     help_info
 fi
 
+if [ x$2 = x"release" ]; then
+    _cppflags_com="${_cppflags_com} -g -O2"
+else
+    _cppflags_com="${_cppflags_com} -g -O0"
+fi
+
 # 3rd_lib path
 prefix_path=${data_disk_path}/install/${vender}/${gcc_version}
 _cppflags_com="${_cppflags_com} -I${prefix_path}/include"
@@ -68,7 +74,7 @@ make distclean
 
 cd `pwd` && ./autogen.sh && cd -
 
-if [ $# = 2 ]; then
+if [ $# = 3 ]; then
     mkdir -p $2/${vender}
     cd $2/${vender}
 fi
