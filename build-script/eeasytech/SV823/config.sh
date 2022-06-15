19bf4da064420210259e44cf1014a5ab2f6ddbc8
zhenquan.qiu <gnsyxiang@163.com>
2022-06-15 16:00:35 +0800

    build: fix voice buf in 'make dist'

--- build-script/eeasytech/SV823/config.sh
+++ build-script/eeasytech/SV823/config.sh
@@ -7,7 +7,7 @@ product=HY-8608 HY-8608E HY-8608EV2
 language=cn en
 
 host=arm-linux-gnueabihf
-cross_gcc_path=/opt/toolchains/eeasytech/SV823/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
+cross_gcc_path=/mnt/data/toolchain/eeasytech/SV823/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf/bin/arm-linux-gnueabihf-
 
 configure_param=
 
